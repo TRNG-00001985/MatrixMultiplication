3a6f3afb2918c1c4a54645fff9c9193fba734c44
Nayini_Archana <nayiniarchana999@gmail.com>
2024-07-26 14:48:34 +0530

    Add files via upload

---
+++ MtrixMultiplication.java
@@ -0,0 +1,17 @@
+package reva;
+
+import java.util.Arrays;
+
+public class MtrixMultiplication {
+	public static void main(String args[]) {
+		int[] arr= {12,34,54,32,12,45};
+		int[] arr2= {32,90,54,21,10,45};
+		int[] res=new int[arr.length];
+		for(int i=0;i<arr.length;i++) {
+			res[i]=arr[i]*arr2[i];
+		}
+		System.out.print(Arrays.toString(res));
+	}
+
+}
+
